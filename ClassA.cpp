e09b57ec1f306add2aac5880ebd96803772e85d2
Nicolas.Braun <nicolas.braun71102@gmail.com>
2020-11-23 14:29:32 +0100

    ClassA.h und ClassA.cpp hinzugefügt

---
+++ ClassA.cpp
@@ -0,0 +1,13 @@
+#include "ClassA.h"
+#include <iostream>
+
+using namespace std;
+
+ClassA::ClassA(){
+
+}
+
+void ClassA::printName()
+{
+    cout<<"My Name is Class A, hello you."<<endl;
+}
---
+++ ClassA.h
@@ -0,0 +1,11 @@
+#ifndef CLASSA_h
+#define CLASSA_h
+
+class ClassA{
+    public:
+        ClassA();
+        void printName();
+    
+
+};
+#endif
